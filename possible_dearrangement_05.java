991d18e82e86ec5dfb64d5b166189c4ff87f8ce2
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-25 20:26:02 +0530

    problem 5

---
+++ possible_dearrangement_05.java
@@ -0,0 +1,17 @@
+public class possible_dearrangement_05 {
+    public static int findDearangment(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 0;
+        }
+        return (n - 1) * (findDearangment(n - 1) + findDearangment(n - 2));
+
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(findDearangment(n));
+    }
+}
