85e8cf7c02957887d8d7f0b8bfb55e04b29519d5
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-27 19:44:38 +0530

    problem added

---
+++ find_missing_number_array_07.java
@@ -0,0 +1,15 @@
+public class find_missing_number_array_07 {
+    public static void findMissing(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i + 1] - arr[i] != 1) {
+                System.out.println((arr[i] + 1));
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 5, 6 };
+        findMissing(arr);
+    }
+}
