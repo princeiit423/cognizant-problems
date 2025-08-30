95d86e50081d445bfcc747b9146729051c9e9efc
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-08-30 20:50:03 +0530

    problems added

--- find_missing_number_array_07.java
+++ find_missing_number_array_07.java
@@ -9,7 +9,7 @@ public class find_missing_number_array_07 {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 4, 5, 6 };
+        int arr[] = { 1, 2, 3, 5, 6 };
         findMissing(arr);
     }
 }
