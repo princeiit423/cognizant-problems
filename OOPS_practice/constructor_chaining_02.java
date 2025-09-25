faad557538b44a4a6606c6147e9435aa162ef9bc
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-09-25 10:36:40 +0530

    consyructor chaining

---
+++ OOPS_practice/constructor_chaining_02.java
@@ -0,0 +1,43 @@
+public class constructor_chaining_02 {
+    static class Customer {
+        private int cId;
+        private String cName;
+        private long cNum;
+
+        Customer() {
+            cId = 1;
+            cName = "afreen";
+            cNum = 896124907;
+        }
+
+        // setter function
+        Customer(int cId, String cName, long cNum) {
+            this(); // used for cunstroctor chaining in local
+            // this.cId = cId;
+            // this.cName = cName;
+            // this.cNum = cNum;
+        }
+
+        // getter fuction
+        public int getId() {
+            return cId;
+        }
+
+        public String getName() {
+            return cName;
+        }
+
+        public long getNum() {
+            return cNum;
+        }
+    }
+
+    public static void main(String args[]) {
+        Customer c1 = new Customer(1, "Alif", 87523190);
+
+        System.out.println(c1.getId());
+        System.out.println(c1.getName());
+        System.out.println(c1.getNum());
+
+    }
+}
