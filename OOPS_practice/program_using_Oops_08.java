818f31162c8787119158a6f4b933334c82094e7c
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-10-02 18:06:33 +0530

    oops problems

--- OOPS_practice/program_using_Oops_08.java
+++ OOPS_practice/program_using_Oops_08.java
@@ -28,11 +28,43 @@ class Square extends Shape {
     }
 }
 
+class Rectangle extends Shape {
+    private float length;
+    private float breadth;
+    private float area;
+
+    public void getInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Length");
+        length = sc.nextFloat();
+        System.out.println("Input breadth: ");
+        breadth = sc.nextFloat();
+    }
+
+    public void calcArea() {
+        area = length * breadth;
+    }
+
+    public void dispArea() {
+        System.out.println(area);
+    }
+}
+
+class Permit {
+    public void allow(Shape ref) {
+        ref.getInput();
+        ref.calcArea();
+        ref.dispArea();
+    }
+}
+
 public class program_using_Oops_08 {
     public static void main(String args[]) {
         Square s = new Square();
-        s.getInput();
-        s.calcArea();
-        s.dispArea();
+        Rectangle r = new Rectangle();
+        Permit p = new Permit();
+        p.allow(s);
+        p.allow(r);
+
     }
 }
