fc72a96827765223b02c5d3c4c5fdfc52e5646d8
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-10-08 19:00:49 +0530

    knapsack

--- fractional_knapsack_19.java
+++ fractional_knapsack_19.java
@@ -1,19 +1,28 @@
-public class fractional_knapsack_19 {
-    public static void findMaxVal(int value[], int weight[], int capacity) {
-        int ratio[] = new int[value.length];
-        int maxVal = 0;
+import java.util.Arrays;
+import java.util.Comparator;
 
+public class fractional_knapsack_19 {
+    public static void findMaxVal(int value[], int weight[], int cap) {
+        int ratio[][] = new int[value.length][2];
         for (int i = 0; i < value.length; i++) {
-            ratio[i] = value[i] / weight[i];
+            ratio[i][0] = i;
+            ratio[i][1] = value[i] / weight[i];
         }
-        for (int i = 0; i < weight.length; i++) {
-            if (capacity > 0) {
-                int addValue = ratio[i] * weight[i];
-                maxVal += addValue;
-                capacity = capacity - addValue;
+        // sort in ascending but we want descending order so we reverse iterate
+        Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+        int capacity = cap;
+        int finalVal = 0;
+        for (int i = ratio.length - 1; i >= 0; i--) {
+            int idx = ratio[i][0];
+            if (capacity >= weight[idx]) {
+                finalVal += value[idx];
+                capacity -= weight[idx];
+            } else {
+                finalVal += (ratio[i][1] * capacity);
+                capacity = 0;
             }
         }
-        System.out.println(maxVal);
+        System.out.println(finalVal);
     }
 
     public static void main(String[] args) {
